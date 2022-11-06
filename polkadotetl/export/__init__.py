adaae5e5b291bec7e2e6ef064d46c1456bf46585
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-11-06 21:06:51 +0530

    feat: add placeholder functions for export blocks
    * add export_blocks_by_number
    * add export_blocks_by_timestamp
    * use these functions within the CLI

--- polkadotetl/export/__init__.py
+++ polkadotetl/export/__init__.py
@@ -3,7 +3,12 @@ from pathlib import Path
 from typing import Optional
 
 from polkadotetl.constants import SIDECAR_RETRIES
-from polkadotetl.export.internals import InputType, validate_inputs
+from polkadotetl.export.internals import (
+    InputType,
+    validate_inputs,
+    export_blocks_by_number,
+    export_blocks_by_timestamp,
+)
 
 
 def export_blocks(
@@ -19,6 +24,18 @@ def export_blocks(
     input_type = validate_inputs(start_block, end_block, start_timestamp, end_timestamp)
 
     if input_type == InputType.BLOCKS:
-        ...
+        export_blocks_by_number(
+            output_directory,
+            sidecar_url,
+            start_block,
+            end_block,
+            retries,
+        )
     else:
-        ...
+        export_blocks_by_timestamp(
+            output_directory,
+            sidecar_url,
+            start_timestamp,
+            end_timestamp,
+            retries,
+        )
--- polkadotetl/export/internals.py
+++ polkadotetl/export/internals.py
@@ -3,10 +3,12 @@ from enum import Enum
 from typing import Optional
 from polkadotetl.logger import logger
 from polkadotetl.exceptions import InvalidInput
+from polkadotetl.constants import SIDECAR_RETRIES
 
 
 class InputType(Enum):
     """Determines which input mode to use."""
+
     BLOCKS = 0
     TIMESTAMP = 1
 
@@ -18,7 +20,6 @@ def validate_inputs(
     end_timestamp: Optional[int] = None,
 ) -> InputType:
     """check the start stop parameters to check whether to use the blocks or the timestamps."""
-
     invalid_input = False
     if start_block is None:
         if start_timestamp is None:
@@ -57,3 +58,37 @@ def validate_inputs(
 def get_block_for_timestamp(timestamp: datetime):
     """Returns a block number for a particular timestamp"""
     # TODO: implement a helper function to get a block number given a timestamp
+    raise NotImplementedError()
+
+
+def export_blocks_by_timestamp(
+    output_directory: Path,
+    sidecar_url: str,
+    start_timestamp: Optional[int] = None,
+    end_timestamp: Optional[int] = None,
+    retries: int = SIDECAR_RETRIES,
+):
+    """Exports blocks from the sidecar by block timestamp"""
+    # TODO: Implement this function
+    # NOTE: Internally calls the export_blocks_by_number
+    start_block = get_block_for_timestamp(start_timestamp)
+    end_block = get_block_for_timestamp(end_timestamp)
+    export_blocks_by_number(
+        output_directory,
+        sidecar_url,
+        start_block,
+        end_block,
+        retries,
+    )
+
+
+def export_blocks_by_number(
+    output_directory: Path,
+    sidecar_url: str,
+    start_block: Optional[int] = None,
+    end_block: Optional[int] = None,
+    retries: int = SIDECAR_RETRIES,
+):
+    """Exports blocks from the sidecar by block number"""
+    # TODO: Implement this function
+    raise NotImplementedError()
